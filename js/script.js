0ac62b3e6a73ef587739adc266a167a02ce09c33
perwebKelompok1 <56380334+perwebKelompok1@users.noreply.github.com>
2019-12-28 11:48:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-function createCookie(cookieName,cookieValue,daysToExpire)
-{
-  var date = new Date();
-  date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
-  document.cookie = cookieName + "=" + cookieValue + "; expires=" + date.toGMTString();
-};
-function accessCookie(cookieName)
-{
-  var name = cookieName + "=";
-  var allCookieArray = document.cookie.split(';');
-  for(var i=0; i<allCookieArray.length; i++)
-  {
-    var temp = allCookieArray[i].trim();
-    if (temp.indexOf(name)==0)
-    return temp.substring(name.length,temp.length);
-     }
-    return "";
-};
-
+        //cookie
+        function createCookie(cookieName,cookieValue,daysToExpire){
+            var date = new Date();
+            date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
+            document.cookie = cookieName + "=" + cookieValue + "; expires="+ date.toGMTString();
+          };
+          function accessCookie(cookieName)
+          {
+            var name = cookieName + "=";
+            var allCookieArray = document.cookie.split(';');
+            for(var i=0; i<allCookieArray.length; i++)
+            {
+              var temp = allCookieArray[i].trim();
+              if (temp.indexOf(name)==0)
+              return temp.substring(name.length,temp.length);
+          }
+              return "";
+          };
+          // cookie
 $(document).ready(function(){
-  
+    $('.materi1 .boxmateri a').click(function(){
+        if(accessCookie("pengguna") == ""){
+            $('.materi1 .boxmateri a').attr('href','#');
+            alert("silahkan login dulu!!!!!");
+        }
+    });
+    $('.logo').addClass('slide-right'); 
     if(accessCookie("pengguna") != ""){
         $('#menuLogin').html("logout");
         $('#menuSign').hide();
+        $('.warning').hide();
     };
-    $(window).ready(function(){
-        $('.logo').addClass('slide-right');
-    });
     $("#menuLogin").click(function(){
         if(accessCookie("pengguna") != ""){
             document.cookie = "pengguna = ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
@@ -41,19 +45,58 @@ $(document).ready(function(){
     $("#pindah").click(function(){
         var mail=$('.inputan input[type="email"]').val(),
             passwd=$('.inputan input[type="password"]').val();
-            if(mail=="vocadu@gmail.com" & passwd=="12345678"){
-                  var user = accessCookie("pengguna");
-                  if (user!=""){
-                    alert("anda suda login " + user + "!!!");
-                  }else
-                  {
-                    createCookie("pengguna", passwd, 1);
-                  }
-                $(location).attr('href','materi.html');
+            if(mail=="vocadu@gmail.com" && passwd=="12345678"){   
+              createCookie("pengguna", mail, 1);
+              $(location).attr('href','materi.html');
     }else{
             alert("username/password salah\n username:vocadu@gmail.com\n password:12345678");
         };
 
     });
+    $('#submit').click(function(){
+      var mail=$('#sign input[type="email"]').val(),
+      passwd=$('#sign input[type="password"]').val();
+      createCookie("pengguna", mail, 1);
+      $(location).attr('href','materi.html');   
+    });
+    $('#more').click(function(){
+        $("#more").hide();
+        $(function() {
+            ajaxJS();
+            function ajaxJS(e) {
+              if (e) {
+                e.preventDefault();
+              }
+              $.ajax({
+                url: "https://raw.githubusercontent.com/suainul1/suainul1/master/suainul.json",
+                method: "GET",
+                success: function(data) {
+                  console.log(data);
+                  data = jQuery.parseJSON(data);
+                  var html_to_append = '';
+                  $.each(data, function(i, item) {
+                    html_to_append +=
+                    '<div class="boxmateri">'+'<img src='+item.img+' '+'alt="">'
+                     + 
+                     '<h4>'+item.judul+'</h4>' +
+                    '<p>'+item.isi+'</p>'+
+                    '<a href='+item.link+'>Baca</a>'+'</div>';
+                  });
+                  
+                  $(".materi1").append(html_to_append);
+                },
+                error: function() {
+                  console.log(data);
+                }
+              });
+        
+            }
+        
+          });
+          $('.materi1 .boxmateri a').attr('href','#');
+    });
+
+    
+
 });
 
